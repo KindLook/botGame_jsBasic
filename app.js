2e64f8e504686748dddf6c886e184d0128d87c10
Slava Panishev <panishev11@gmail.com>
2021-04-11 18:52:20 +0300

    Задание к уроку 6, игровой чатбот, обязательный. Все работает, так и не понял как замкнуть...

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 'use strict';
 
+let isNumber = function (n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+function quessAnswer(){
+    let question = prompt('Угадай число от 1 до 100');
+    let answer = 30;
+    if(question === null){
+        alert('Игра закончена');
+    }
+    else if (question == answer){
+        alert('Поздравляю, Вы угадали!!!')
+    }
+    else if(!isNumber(question)){
+        alert('Введи число!');
+        quessAnswer();
+    }
+     else if(question > answer){
+        alert('Загаданное число меньше');
+        quessAnswer();
+    }
+     else if(question < answer){
+        alert('Загаданное число больше');
+        quessAnswer();
+     }
+    }
+
+quessAnswer();
+
